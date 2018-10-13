8ce100850862b26d46597677f143d314f2d31131
Gaël Berthaud-Müller <blacksponge@tuta.io>
2018-10-14 01:18:57 +0200

    Add creation of new user on start

--- run-flood.sh
+++ run-flood.sh
@@ -12,6 +12,24 @@ if [ ! -f /flood/.done ]; then
     npm run build
   fi
 
+  node << EOF
+const Users = require('./server/models/Users')
+Users.createUser({
+  username: process.env.FLOOD_USER,
+  password: process.env.FLOOD_PASSWORD,
+  host: process.env.RTORRENT_SCGI_HOST,
+  port: process.env.RTORRENT_SCGI_PORT,
+  socketPath: null,
+  isAdmin: true}, (user, err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log(user)
+    process.exit(0)
+  });
+EOF
+
   touch /flood/.done
 fi
 
